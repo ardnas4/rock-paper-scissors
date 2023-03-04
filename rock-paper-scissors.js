9b18f5817a1dbb5f7bbb2b8cb3a8f7498cd5d8d2
Sandra <sandramai432@gmail.com>
2023-03-04 12:44:11 -0500

    mostly done js logic. no fancy css yet

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,30 @@
 let computerScore = 0, playerScore = 0;
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', event => {
+        if (computerScore == 5 || playerScore == 5) {
+            return;
+        }
+
+        let computerSelection = getComputerChoice();
+        let playerSelection = event.target.id;
+
+        playRound(playerSelection, computerSelection);
+        determineWinner();
+    });
+});
+
 function getComputerChoice() {
     // list of possible choices
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
 
     // random choice
     let result = Math.floor(Math.random() * choices.length);
+
+    // display the computer's choice 
+    document.getElementById("computer-choice").innerHTML = "The computer chose: " + choices[result];
 
     // returning the actual string chocie
     return choices[result];
@@ -18,65 +37,60 @@ function playRound(playerSelection, computerSelection) {
 
     else if (playerSelection.toLowerCase() == 'rock') {
         if (computerSelection.toLowerCase() == 'paper') {
-            document.getElementById("result").innerHTML = 'You lose! Rock loses to paper.';
+            document.getElementById("result").innerHTML = 'You lose! Rock loses to Paper.';
             computerScore++;
         }
 
         else if (computerSelection.toLowerCase() == 'scissors') {
-            document.getElementById("result").innerHTML = 'You Win! Rock beats scissors.';
+            document.getElementById("result").innerHTML = 'You Win! Rock beats Scissors.';
             playerScore++;
         }
     }
 
     else if (playerSelection.toLowerCase() == 'paper') {
         if (computerSelection.toLowerCase() == 'rock') {
-            document.getElementById("result").innerHTML = 'You win! Paper beats rock.';
+            document.getElementById("result").innerHTML = 'You win! Paper beats Rock.';
             playerScore++;
         }
 
         else if (computerSelection.toLowerCase() == 'scissors') {
-            document.getElementById("result").innerHTML = 'You lose! Paper loses to scissors.';
+            document.getElementById("result").innerHTML = 'You lose! Paper loses to Scissors.';
             computerScore++;
         }
     }
 
     else if (playerSelection.toLowerCase() == 'scissors') {
         if (computerSelection.toLowerCase() == 'rock') {
-            document.getElementById("result").innerHTML = 'You lose! Scissors loses to rock.';
+            document.getElementById("result").innerHTML = 'You lose! Scissors loses to Rock.';
             computerScore++;
         }
 
         else if (computerSelection.toLowerCase() == 'paper') {
-            document.getElementById("result").innerHTML = 'You win! Scissors beat paper.';
+            document.getElementById("result").innerHTML = 'You win! Scissors beat Paper.';
             playerScore++;
         }
     }
+
+    updateScoreboard();
+}
+
+function updateScoreboard() {
+    document.getElementById("scoreboard").innerHTML = playerScore + "     -     " + computerScore;
 }
 
 function determineWinner() {
-    const container = document.querySelector('body');
+    const element = document.getElementById("weapon-choice");
+
     const winner = document.createElement('div');
     winner.id = 'winner';
 
     if (playerScore == 5) {
-        winner.textContent = 'The player has won 5 games first!';
+        winner.textContent = 'Congratulations! You win!';
     }
 
     else if (computerScore == 5) {
-        winner.textContent = 'The computer has won 5 games first!';
+        winner.textContent = 'Boooo! The computer wins!';
     }
 
-    container.appendChild(winner);
+    element.after(winner);
 }
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-    button.addEventListener('click', event => {
-        let computerSelection = getComputerChoice();
-        let playerSelection = event.target.id;
-
-        playRound(playerSelection, computerSelection);
-        determineWinner();
-    });
-});
